b8d77f9c31e96dcdb00cbe899d5bd371cc1d0e32
Gibson Akwasi Opoku <83539684+badboigibby@users.noreply.github.com>
2025-09-01 17:10:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -63,5 +63,5 @@ def chat():
 # Run App
 # ----------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Railway injects PORT
+    port = int(os.environ.get("PORT"))
     app.run(host="0.0.0.0", port=port)
